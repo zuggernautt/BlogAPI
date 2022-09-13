ea28211cb19894e417fed50da9d6c6a2278b1768
zuggernautt <amitak0707@gmail.com>
2022-09-13 21:10:22 +0530

    Init

---
+++ posts/views.py
@@ -0,0 +1,16 @@
+from rest_framework import generics
+from .models import Post
+from .serializers import PostSerializer
+from .permissions import IsAuthorOrReadOnly
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+# Authorization not working properly
+# make test user 2 to check token auth
